106f562efde8dab6b55329b825ad3addddffae8a
RaykoRadev <christmas898@gmail.com>
2025-09-17 14:35:39 +0200

    adding page not found view

--- routes.js
+++ routes.js
@@ -6,4 +6,14 @@ router.get("/", (req, res) => {
     res.render("home");
 });
 
+router.get("/create", (req, res) => {});
+
+router.get("/details/:id ", (req, res) => {});
+
+router.get("/about", (req, res) => {});
+
+router.get("*spat", (req, res) => {
+    res.render("404");
+});
+
 export default router;
