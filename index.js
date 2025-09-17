74849aa4791cb54a5a2c782ca9ec5b7beae53f0c
RaykoRadev <christmas898@gmail.com>
2025-09-17 14:29:54 +0200

    rendering home page

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { hbs } from "./hbsConfig.js";
+// import { hbs } from "./hbsConfig.js";
 import router from "./routes.js";
+import handlebars from "express-handlebars";
 
 const app = express();
+
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname: "hbs",
+    })
+);
 app.set("view engine", "hbs");
 
 app.use(express.static("public"));
