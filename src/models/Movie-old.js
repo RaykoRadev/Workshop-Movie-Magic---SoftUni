1ee401993da9b517cf5cef51e97550db8f422312
RaykoRadev <christmas898@gmail.com>
2025-09-25 15:27:19 +0200

    import files to DB

--- src/models/Movie-old.js
+++ src/models/Movie-old.js
@@ -1,55 +1,55 @@
-import fs from "fs/promises";
-import { v4 as uuid } from "uuid";
+// import fs from "fs/promises";
+// import { v4 as uuid } from "uuid";
 
-let dbSerialized = await fs.readFile("./src/movie-data.json", {
-    encoding: "utf-8",
-});
-let db = JSON.parse(dbSerialized);
+// let dbSerialized = await fs.readFile("./src/movie-data.json", {
+//     encoding: "utf-8",
+// });
+// let db = JSON.parse(dbSerialized);
 
-// console.log(db);
+// // console.log(db);
 
-export default class Movie {
-    constructor(data) {
-        Object.assign(this, data);
+// export default class Movie {
+//     constructor(data) {
+//         Object.assign(this, data);
 
-        this.id = uuid();
-    }
+//         this.id = uuid();
+//     }
 
-    static findMovies(criteria = {}) {
-        let result = db.movies;
+//     static findMovies(criteria = {}) {
+//         let result = db.movies;
 
-        console.log(criteria);
+//         console.log(criteria);
 
-        if (criteria.title) {
-            result = db.movies.filter((m) =>
-                m.title.toLowerCase().includes(criteria.title.toLowerCase())
-            );
-        }
+//         if (criteria.title) {
+//             result = db.movies.filter((m) =>
+//                 m.title.toLowerCase().includes(criteria.title.toLowerCase())
+//             );
+//         }
 
-        if (criteria.genre) {
-            result = db.movies.filter(
-                (m) => m.genre.toLowerCase() === criteria.genre.toLowerCase()
-            );
-        }
+//         if (criteria.genre) {
+//             result = db.movies.filter(
+//                 (m) => m.genre.toLowerCase() === criteria.genre.toLowerCase()
+//             );
+//         }
 
-        if (criteria.year) {
-            result = db.movies.filter((m) => m.year === criteria.year);
-        }
+//         if (criteria.year) {
+//             result = db.movies.filter((m) => m.year === criteria.year);
+//         }
 
-        return result;
-    }
+//         return result;
+//     }
 
-    static getMovieById(movieId) {
-        return db.movies.find((m) => m.id === movieId);
-    }
+//     static getMovieById(movieId) {
+//         return db.movies.find((m) => m.id === movieId);
+//     }
 
-    async save() {
-        db.movies.push(this);
+//     async save() {
+//         db.movies.push(this);
 
-        const dbDeserialized = JSON.stringify(db, null, 2);
+//         const dbDeserialized = JSON.stringify(db, null, 2);
 
-        await fs.writeFile("./src/movie-data.json", dbDeserialized);
+//         await fs.writeFile("./src/movie-data.json", dbDeserialized);
 
-        return this;
-    }
-}
+//         return this;
+//     }
+// }
