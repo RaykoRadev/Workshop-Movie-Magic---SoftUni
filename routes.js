4113aabc663d91aec0f8dda1aa4cb6d20261715f
RaykoRadev <christmas898@gmail.com>
2025-10-03 09:56:40 +0200

    creating auth controller

--- routes.js
+++ routes.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import homeController from "./src/controllers/homeController.js";
 import movieController from "./src/controllers/movieControler.js";
 import castController from "./src/controllers/castController.js";
+import authController from "./src/controllers/authController.js";
 
 const routers = Router();
 
 routers.use(homeController);
 routers.use("/movies", movieController);
 routers.use("/casts", castController);
+routers.use("/auth", authController);
 
 routers.get("*spat", (req, res) => {
     res.render("404");
